f8302e2d60b59e73ffce2e400fac1c36bf8ab6f7
Craig Wright <craigers.wright@gmail.com>
2021-07-01 00:58:51 -0600

    Initail files

---
+++ stack_problem_1.py
@@ -0,0 +1,60 @@
+"""
+Craig Wright
+CSE 212
+Spring 2021
+
+Below is a simple program of a digital to-do list for Dory, a very forgetful fish who somehow landed an office job.
+(The extensive modifications needed for a fish's office cubicle are beyond the scope of this text.)
+
+Dory can never remember to do things in chronological order, so she just does what is on the top of her to-do list. Once
+she completes an item, it needs to be removed from her list. But if Dory happens on some luck and remembers something
+randomly, she needs to be able quickly add it to her to-do list so that it can be written down before her memory is
+wiped clean again.
+
+In this problem, there is an error that causes the to-do list to not update correctly. Edit the code so that the program
+will function correctly.
+"""
+
+
+def add_task(checklist, task):
+    checklist.pop(task)
+
+
+def complete_and_report_task(checklist):
+    if len(checklist) >= 0:
+        print(checklist.pop())
+
+
+# Test cases:
+# Test cases:
+to_do_list = ['Just keep swimming']
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, 'Find Nemo!')
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, 'Talk Marlin\'s ear off')
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, 'Help the kind orange fish find his son!')
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, 'Learn to speak whale')
+complete_and_report_task(to_do_list)  # Learn to speak whale
+complete_and_report_task(to_do_list)  # Just keep swimming
+complete_and_report_task(to_do_list)  # Help the kind orange fish find his son!
+complete_and_report_task(to_do_list)  # Just keep swimming
+add_task(to_do_list, 'Just keep swimming')
+add_task(to_do_list, '42 Wallaby Way. Sydney')
+add_task(to_do_list, 'Make friends with sharks')
+complete_and_report_task(to_do_list)  # Make friends with sharks
+add_task(to_do_list, 'Get stung by jelly fish')
+complete_and_report_task(to_do_list)  # Get stung by jelly fish
+complete_and_report_task(to_do_list)  # 42 Wallaby Way. Sydney
+complete_and_report_task(to_do_list)  # Just keep swimming
+complete_and_report_task(to_do_list)  # Just keep swimming
+add_task(to_do_list, 'Ride on the backs of rasta sea turtles')
+complete_and_report_task(to_do_list)  # Ride on the backs of rasta sea turtles
+complete_and_report_task(to_do_list)  # Talk Marlin's ear off
+complete_and_report_task(to_do_list)  # Just keep swimming
+complete_and_report_task(to_do_list)  # Find Nemo!
+complete_and_report_task(to_do_list)  # Just keep swimming
+complete_and_report_task(to_do_list)  # Just keep swimming
+complete_and_report_task(to_do_list)  # no output
---
+++ stack_solution_2.py
@@ -0,0 +1,62 @@
+"""
+Craig Wright
+CSE 212
+Spring 2021
+
+Its 2458 and inflation is at ridiculous levels never seen before in the history of the Milky Way galaxy. You have an
+annual salary of $27,200,843.04 which is now average for the middle class. You get paid $1,133,368.46 bi-monthly plus
+any bonuses or overtime you earned during that period.
+
+You have kept track of your paychecks for the first 5 months of the year by adding them to an array. (It doesn't make
+the most sense, but intergalactic law now requires this strange format.) The problem is that January's statements are at
+the front of the array. To make things more confusing and annoying, federal law in 2458 prohibits the reading of arrays
+from the beginning for some bureaucratic reason that we don't pretend to understand.
+
+To keep in line with the laws and avoid space jail, create a program that will use stack memory and functions to reverse
+your statements so that they can be printed out in chronological order (reverse of what they are now).
+"""
+
+
+def reorder_statements(list_to_reorder):
+    # Create a new stack.
+    ordered_statements = []
+
+    # Add the data from the old list to the new stack to reorder it.
+    for counter in range(len(list_to_reorder)):
+        ordered_statements.append(list_to_reorder.pop())
+
+    # Return the new stack to the main program.
+    return ordered_statements
+
+
+def print_statement(list_to_display):
+    for counter in range(len(list_to_display)):
+
+        # Removes and returns the last stored value in the stack.
+        item = list_to_display.pop()
+
+        # Formats the string with the data from the tuple.
+        print(f"Your {item[0]} statement records ${item[1]} in earnings.")
+
+
+# Tuple -->(Month and statement number, earned amount).
+statements = [('Jan1', 1133599.99), ('Jan2', 1137896.01), ('Feb1', 1200000.02), ('Feb2', 1133369.03),
+              ('Mar2', 1133400.04), ('Apr1', 1133389.05), ('Apr2', 1730336.06), ('May1', 1135368.07),
+              ('May2', 1144432.08)]
+
+# Reorder the statements.
+new_statements = reorder_statements(statements)
+
+# Display the statements.
+print_statement(new_statements)
+
+# Verify output:
+# Your Jan1 statement records $1133599.99 in earnings.
+# Your Jan2 statement records $1137896.01 in earnings.
+# Your Feb1 statement records $1200000.02 in earnings.
+# Your Feb2 statement records $1133369.03 in earnings.
+# Your Mar2 statement records $1133400.04 in earnings.
+# Your Apr1 statement records $1133389.05 in earnings.
+# Your Apr2 statement records $1730336.06 in earnings.
+# Your May1 statement records $1135368.07 in earnings.
+# Your May2 statement records $1144432.08 in earnings.
