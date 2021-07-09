1a4316c4cc3f40476a1d74c37c5fc41279110eb0
Craig Wright <craigers.wright@gmail.com>
2021-07-09 14:54:24 -0600

    Linked List added

---
+++ Linked List/linked_list_final.md
@@ -0,0 +1,93 @@
+# Linked Lists
+A linked list is another very useful type of data structure. In Python, an array is called a “list” so one would assume
+that a linked list would be very similar because they are both “lists”. That is incorrect as the only applicable
+similarity that a Python “list” (array) shares with a linked list is the ability to store data in memory. A fundamental
+principle of arrays is that they are contiguous in memory. This is not so with linked lists.
+
+![Linked List Representation](Linked List.png)
+
+Each item in a linked list can be anywhere in memory. Because of this, each item has “directions” that guide to the next
+item in the list. These directions are contained in each “link” called a **node**. A node contains two parts, the data
+stored and a memory address book that contains address for the previous and following nodes.
+
+The nature of the linked list makes it impossible to jump directly to an item in the list. One must start from either
+the front, called the head, or the tail of the list and iterate through each “link” until the desired data is found.
+Because of this, looking up a value in a linked list is O(n).
+
+## Inserting into a Linked List
+In a dynamic array, inserting or removing data anywhere but the end is O(n) because of the sequential nature of memory
+used to store the data causes all the elements stored to shift to accommodate the change.
+
+A linked list is always O(1) to insert or remove from the head or tail, but it is O(n) to removing from the middle,
+because you must iterate through the list to find the node you desire to add/remove. When one adds or removes an
+element, care needs to be taken in the process since each element contains the addresses of the next and previous
+elements.
+
+#### Syntax
+If there are no elements in a linked list, then creating a new list is simple. First, let's go over some helpful syntax
+for working with linked lists:
+**_self.head_** – this is the syntax that refers to the beginning element in a linked list.
+
+**_self.tail_** -- this is the syntax that refers to the last element in a linked list.
+
+**_self.next_** – for our purposes, _self.next_ is the syntax that points to the next element in the list.
+_ex. self.head.next_ refers to the element immediately following the head.
+
+**_self.prev_** _–_ for our purposes, _self.prev_ is the syntax that points to the previous element in the list.
+_ex. self.tail.prev_ refers to the element immediately before the tail. **Note: _self.head.prev_ and _self.tail.next_
+should always equal _None_ since they are on the ends of the linked list.**
+
+**_self.data_** – this is the syntax that allows one to access the data stored in the node.
+
+#### Creating a New Linked List
+If there are no elements in a linked list, then creating a new list is simple. First, we need to create a new node and
+set it to be both the head and the tail:
+
+1. Set the new_node as the head: _self.head = new_node_
+2. Set the new_node as the tail: _self.tail = new node_
+
+![Creating a New Linked List](create new node.png)
+
+Now what if we want to add another element at the end and create a new tail?
+
+![Create a New Tail](insert new tail.png)
+
+Let us call the new element _new_tail._
+1. We first need to connect new_tail to the current tail: _new_tail.prev = self.tail_
+2. Now we need to attach the current tail to new_tail: _self.tail.next = new_tail_
+3. Now we need to make new_tail the current tail: _self.tail = new_tail_
+
+ The order here is very important. If these steps are done out of order you could lose the address of the element that
+ had been before the original tail. It is important to attach the new node before removing the existing ties between
+ nodes. This ensures that we still have links to all the data.
+ 
+What if we want to insert a new head?
+
+![Insert a New Head](insert new head.png)
+
+The idea is very similar to adding a tail. We will call the new element _new_head_:
+1. We first need to connect new_tail to the current tail: _new_head.next = self.head_
+2. Now we need to attach the current tail to the new_tail: _self.head.prev = new_head_
+3. Now we need to make new_head the current head: _self.head = new_head_
+
+What about adding in the middle of the linked list? This is a little trickier as the middle of the linked list may not
+be directly linked to either the head or the tail. This means we will have to iterate through the list to find the
+location we will want to insert at.
+
+The concept is the same as inserting a head or tail, but instead of attaching the new node to the head or tail, we will
+be connecting the new node to the nodes that come both before and after. We will call the new node _new_element_ and
+_curr_ refers to the current node:
+
+1. We first need to connect new_element to the current node: _new_element.prev = curr_
+2. Now we need to attach new_element to the next element: _new_element.next = curr.next_
+3. Now we need to connect the next node to new_element: _curr.next.prev = element_
+4. Now connect the current node to the new_element: _curr.next = new_element_
+
+
+![Insert in Middle](insert new node.png)
+
+In the above picture, the red arrows show how the new node was inserted into the linked list before the tail. The 2nd
+node and the tail do not point to each other anymore, rather they now provide an address to the new node.
+
+##Problem Set
+
---
+++ Linked List/linked_list_problems.py
@@ -0,0 +1,256 @@
+"""
+CSE212
+(c) BYU-Idaho
+07-Prove - Problems
+
+It is a violation of BYU-Idaho Honor Code to post or share this code with others or
+to post it online.  Storage into a personal and private repository (e.g. private
+GitHub repository, unshared Google Drive folder) is acceptable.
+"""
+
+
+class LinkedList:
+    """
+    Implement the LinkedList data structure.  The Node class below is an
+    inner class.  An inner class means that its real name is related to
+    the outer class.  To create a Node object, we will need to
+    specify LinkedList.Node
+    """
+
+    class Node:
+        """
+        Each node of the linked list will have data and links to the
+        previous and next node.
+        """
+
+        def __init__(self, data):
+            """
+            Initialize the node to the data provided.  Initially
+            the links are unknown so they are set to None.
+            """
+            self.data = data
+            self.next = None
+            self.prev = None
+
+    def __init__(self):
+        """
+        Initialize an empty linked list.
+        """
+        self.head = None
+        self.tail = None
+
+    def insert_head(self, value):
+        """
+        Insert a new node at the front (i.e. the head) of the
+        linked list.
+        """
+        # Create the new node
+        new_node = LinkedList.Node(value)
+
+        # If the list is empty, then point both head and tail
+        # to the new node.
+        if self.head is None:  # TODO: fix this part of the function
+            self.head = new_node
+            self.head = new_node
+        # If the list is not empty, then only self.head will be
+        # affected.
+        else:
+            new_node.next = self.head  # Connect new node to the previous head
+            self.head.prev = new_node  # Connect the previous head to the new node
+            self.head = new_node  # Update the head to point to the new node
+
+    def insert_tail(self, value):
+        """
+        Insert a new node at the back (i.e. the tail) of the
+        linked list.
+        """
+        # Create a new node
+        new_tail = LinkedList.Node(value)
+        # If the list is empty, then point both head and tail
+        # to the new node.
+        if self.tail is None:  # TODO: fix this part of the function
+            self.tail = new_tail
+            self.tail = new_tail
+
+            # If the list is not empty, then only self.tail will be
+            # affected.
+        else:
+            new_tail.prev = self.tail  # Connect new node to the previous tail
+            self.tail.next = new_tail  # Connect the previous tail to the new node
+            self.tail = new_tail  # Update the tail to point to the new node
+
+    def remove_head(self):
+        """
+        Remove the first node (i.e. the head) of the linked list.
+        """
+        # If the list has only one item in it, then set head and tail
+        # to None resulting in an empty list.  This condition will also
+        # cover an empty list.  Its okay to set to None again.
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        # If the list has more than one item in it, then only self.head
+        # will be affected.
+        elif self.head is not None:
+            self.head.next.prev = None  # Disconnect the second node from the first node
+            self.head = self.head.next  # Update the head to point to the second node
+
+    def remove_tail(self):
+        """
+        Remove the last node (i.e. the tail) of the linked list.
+        """
+        # If there is only one item in linked list
+        # set both head and tail to None
+        if self.tail == self.head:
+            self.head = None
+            self.tail = None
+        # If there is more than one item in the list
+        # remove the last item
+        elif self.tail is not None:
+            self.tail.prev.next = None
+            self.tail = self.tail.prev
+
+    def insert_after(self, value, new_value):
+        """
+        Insert 'new_value' after the first occurrence of 'value' in
+        the linked list.
+        """
+        # Search for the node that matches 'value' by starting at the
+        # head of the list.
+        curr = self.head
+        while curr is not None:
+            if curr.data == value:
+                # If the location of 'value' is at the end of the list,
+                # then we can call insert_tail to add 'new_value'
+                if curr == self.tail:
+                    self.insert_tail(new_value)
+                # For any other location of 'value', need to create a
+                # new node and reconnect the links to insert.
+                else:
+                    new_node = LinkedList.Node(new_value)
+                    # TODO: implement this part of the function
+                    pass
+
+                return  # We can exit the function after we insert
+            curr = curr.next  # Go to the next node to search for 'value'
+
+    def remove(self, value):
+        """
+        Remove the first node that contains 'value'.
+        """
+        # Search for the node that matches 'value' by starting at the
+        # head of the list.
+        curr = self.head
+        while curr is not None:
+            if curr.data == value:
+                # If the value is contained in the head of the list, call the remove head function
+                if curr == self.head:
+                    self.remove_head()
+                # If the value is contained in the tail of the list, call the remove tail function
+                elif curr == self.tail:
+                    self.remove_tail()
+                # If the value is contained anywhere in the middle of the list, remove any pointers to it
+                else:  # TODO: fix this part of the function
+                    curr.prev = curr.next
+                    curr.next = curr.prev
+                # Once replaced, exit the function
+                return
+            curr = curr.next
+
+    def replace(self, old_value, new_value):
+        """
+        Search for all instances of 'old_value' and replace the value
+        to 'new_value'.
+        """
+        # Search for the node that matches 'value' by starting at the
+        # head of the list.
+        curr = self.head
+        while curr is not None:
+            # Once value is found, replace the old_value to the new_value
+            if curr.data == old_value:
+                curr.data = new_value
+            # Makes sure to go through the entire list and replace all old_values
+            curr = curr.next
+
+    def __iter__(self):
+        """
+        Iterate forward through the Linked List
+        """
+        curr = self.head  # Start at the beginning since this is a forward iteration.
+        while curr is not None:
+            yield curr.data  # Provide (yield) each item to the user
+            curr = curr.next  # Go forward in the linked list
+
+    def __reversed__(self):
+        """
+        Iterate backward through the Linked List
+        """
+        curr = self.tail  # Start at the end since this is a reverse iteration.
+        while curr is not None:
+            yield curr.data  # Provide (yield) each item to the user
+            curr = curr.prev  # Go backward in the linked list
+
+    def __str__(self):
+        """
+        Return a string representation of the linked list.
+        """
+        output = "linkedlist["
+        first = True
+        for value in self:
+            if first:
+                first = False
+            else:
+                output += ", "
+            output += str(value)
+        output += "]"
+        return output
+
+
+# Sample Test Cases
+ll = LinkedList()
+# TODO: PROBLEM 1: We are wanting to create a linked list of the alphabet. For problem one, fix the code to implement
+# TODO: the head and tail of the linked list.
+
+ll.insert_tail('D')
+ll.insert_head('C')
+print(ll)  # [C, D]
+ll.insert_head(5)
+ll.insert_tail('f')
+ll.insert_head('B')
+ll.insert_tail('X')
+print(ll)  # [B, 5, C, D, f, X]
+
+# TODO: PROBLEM 2: The linked list is in alphabetical order at the moment except the 5 that made its way in the list.
+# TODO: Fix the 'remove' function so that you can remove the 5 from the list.
+ll.remove(5)
+ll.replace('f', 'F')
+ll.insert_head('A')
+ll.insert_tail('Z')
+print(ll)  # [A, B, C, D, F, X, Z]
+
+#  TODO: PROBLEM 3: Implement the 'insert after' function to add the rest of the alphabet to the list so that the list
+#  TODO: is in alphabetical order.
+
+ll.insert_after('D', 'E')
+ll.insert_after('F', 'I')
+ll.insert_after('F', 'G')
+ll.insert_after('G', 'H')
+ll.insert_after('I', 'J')
+ll.insert_after('J', 'K')
+ll.insert_after('K', 'P')
+ll.insert_after('K', 'O')
+ll.insert_after('K', 'N')
+ll.insert_after('K', 'L')
+ll.insert_after('L', 'M')
+ll.insert_after('X', 'Y')
+print(ll)  # [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, X, Y, Z]
+ll.insert_after('P', 'Q')
+ll.insert_after('Q', 'R')
+ll.insert_after('R', 'S')
+ll.insert_tail('Next time won\'t you sing with me')
+ll.insert_after('S', 'T')
+ll.insert_after('T', 'U')
+ll.insert_after('U', 'W')
+ll.insert_after('U', 'V')
+print(ll)  # [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+# Next time won't you sing with me]
